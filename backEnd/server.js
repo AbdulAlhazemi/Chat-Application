9ec70ffc4b18a3dd25a2f467a5079937f8b65b5b
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-10-27 22:45:09 +0300

    Update server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,23 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-import authRoutes from './routes/auth.js'
-import connectToMongoDB from './db/connectToMangoDB.js'
-
+import authRoutes from './routes/auth.js';
+import connectToMongoDB from './db/connectToMangoDB.js';
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 4000;
+
+// Middleware to parse JSON
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-  //root endpoint http://localhost:${PORT}
-  res.send('hello world')
-})
-
+// Use authentication routes
 app.use('/api/auth', authRoutes);
 
+// Root endpoint
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Connect to MongoDB and start the server
 app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on http://localhost:${PORT}`);
