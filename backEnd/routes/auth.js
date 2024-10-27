a3c11785721c9693349d964dd4d6a29397d78846
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-10-27 22:21:44 +0300

    Update auth.js

--- backEnd/routes/auth.js
+++ backEnd/routes/auth.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import { signUpUser, loginUser, logoutUser } from '../controllers/controllers.js';
 
 const router = express.Router();
 
-router.post("/api/auth/signup", signup);
+router.post("/api/auth/signup", signUpUser);
 
-router.post("/api/auth/login", login);
+router.post("/api/auth/login", loginUser);
 
-router.post("/api/auth/logout", logout);
+router.post("/api/auth/logout", logoutUser);
 
 export default router
