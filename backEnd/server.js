5998da2c2baa9b3a4da8a1568b238935ab302229
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-10-27 21:52:10 +0300

    Update server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,17 +1,24 @@
-import express from 'express'; 
+import express from 'express';
 import dotenv from 'dotenv';
+
 import authRoutes from './routes/auth.js'
+import connectToMongoDB from './db/connectToMangoDB.js'
+
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
 
-app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   //root endpoint http://localhost:${PORT}
+  res.send('hello world')
 })
 
+app.use('/api/auth', authRoutes);
+
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
