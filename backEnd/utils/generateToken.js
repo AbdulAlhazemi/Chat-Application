7dbb9d913e567a294dec00a30ae1bb4a2cc072c3
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-10-27 23:07:47 +0300

    Update generateToken.js

---
+++ backEnd/utils/generateToken.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+// Function to generate a JWT token
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '15d', // Token expiration time
+  });
+};
+
+// Function to set the JWT token as a cookie
+const setTokenCookie = (token, res) => {
+  res.cookie("jwt", token, { // Correct cookie name
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
+    httpOnly: true, // Prevent XSS attacks
+    sameSite: 'strict', // Prevent CSRF attacks
+  });
+};
+
+export { generateToken, setTokenCookie };
