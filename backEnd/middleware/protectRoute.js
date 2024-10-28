95a40a153056e51a307a8abcd96e52ae8e61f1c5
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-10-28 19:48:46 +0300

    Create protectRoute.js

---
+++ backEnd/middleware/protectRoute.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/user.model';
+
+const protectRoute = async(req, res, next) => {
+  try {
+    const token = req.cookie.jwt;
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorised - No token provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET,);
+    if (!decoded) {
+      return res.status(401).json({ error: 'Unauthorised - invalide token' });
+    }
+    const user = await User.findById(decoded.userId).select('-password');
+    if(!user) {
+      return res.status(404).json({ error: "User not found"})
+    }
+    req.user = user
+  } catch (error) {
+    console.log('Error in protectRoute middileWare:', error.massage)
+  }
+}
+
+export default protectRoute
