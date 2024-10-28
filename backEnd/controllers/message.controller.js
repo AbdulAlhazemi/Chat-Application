4dd169c09fd6a8519cdb0e0324231f7efa2c851e
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-10-29 00:05:52 +0300

    Update message.controller.js

--- backEnd/controllers/message.controller.js
+++ backEnd/controllers/message.controller.js
@@ -19,9 +19,14 @@ export const sendMessage = async (req, res) => {
       message,
     })
 
+    if (newMessage) {
+      conversation.message.push(newMessage._id);
+    }
+
+    res.status(201).json({ message: "Message sent successfully" })
+
   } catch (error) {
     console.log('Error in sendMssage controller:', error);
     res.status(500).json({ error: 'internal server Error' });
   }
 }
-
