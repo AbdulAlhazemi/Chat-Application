7a46362683d3eb40502b552113e75499d65be9b2
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-10-28 19:48:49 +0300

    Update massage.js

--- backEnd/routes/massage.js
+++ backEnd/routes/massage.js
@@ -1,8 +1,9 @@
 import express from 'express';
-
+import { sendMassage } from '../controllers/massage.controller.js';
+import protectRoute from '../middleware/protectRoute.js';
 const router = express.Router();
 
-router.post("/send/:id", sendMassage);
+router.post("/send/:id", protectRoute, sendMassage);
 
 
 export default router;
