c1ceef6db6450886246f11262d028fb85d2c1b1f
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-10-30 00:19:09 +0300

    Create user.routes.js

---
+++ backEnd/routes/user.routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+import { getUsersForSidebar } from '../controllers/user.controllers';
+import protectRoute from '../middleware/protectRoute';
+
+
+const router = express.Router();
+
+
+router.get("/", protectRoute, getUsersForSidebar);
+
+
+export default router;
