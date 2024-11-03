4cb9baa45a07826a0811ec8a7b2590bdc0a6fe30
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-11-03 19:47:13 +0300

    Update server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,13 +4,15 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
 import massageRoute from './routes/massage.routes.js';
-//import userRoutes from './routes/user.routes.js';
+import userRoutes from './routes/user.routes.js';
 
 
 import connectToMongoDB from './db/connectToMangoDB.js';
 
 const app = express();
 dotenv.config();
+
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 4000;
 
 // Middleware to parse JSON
@@ -20,11 +22,14 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/massages', massageRoute);
-//app.use('/api/users', userRoute);
+app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 
 // Root endpoint
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 // Connect to MongoDB and start the server
