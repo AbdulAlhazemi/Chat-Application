4c8e10f09948f3aa02091881af90606513f7b2f0
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-11-05 16:08:54 +0300

    Update server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -15,28 +15,25 @@ dotenv.config();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 4000;
 
-
-// Middleware to parse JSON
+// Middleware setup
 app.use(express.json());
 app.use(cookieParser());
 
-
+// API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/massages', massageRoutes);
 app.use('/api/users', userRoutes);
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "../frontend/index.html")));
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-
-// Root endpoint
+// Root endpoint for SPA
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-
 // Connect to MongoDB and start the server
 server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
 });
